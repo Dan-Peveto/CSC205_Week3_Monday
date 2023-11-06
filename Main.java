2c815f885178dac9d05d5ba6d83374b52f11ebe7
dan-peveto <daniel_peveto@outlook.com>
2023-11-06 15:18:26 -0800

    Updates to Main, Student, enum, and car

--- Main.java
+++ Main.java
@@ -7,13 +7,15 @@ public class Main {
     
     public static void main(String[] args) {
         SeasonsLogig();
+        CarLogic();
+        StudentLogic();
 
-        Student student1 = new Student();
-        student1.FirstName = "Kurt";
-        student1.LastName = "Vonnegut";
-        student1.GPA = 4.0f;
-        student1.Major = Major.ART; 
-        System.out.println(student1.FirstName);
+        
+
+        
+       
+        
+
     }
     public static void SeasonsLogig() {
         Seasons seasons = Seasons.SPRING; 
@@ -25,5 +27,33 @@ public class Main {
         } else {
             System.out.println("Yea this one is okay I guess");
         }
+    }
+    public static void CarLogic() {
+        Car car1 = new Car();
+        car1.Milage = 50000;
+        car1.Bodystyle = BodyStyle.SEDAN;
+        car1.CarMake = Make.FORD;
+        Car car2 = new Car();
+        car2.Milage = 75000;
+        car2.Bodystyle = BodyStyle.VAN;
+        car2.CarMake = Make.TOYOTA;
+        Car car3 = new Car();
+        car3.Milage = 125000;
+        car3.Bodystyle = BodyStyle.TRUCK;
+        car3.CarMake = Make.GMC;
+        System.out.println(car1.CarMake);
+        System.out.println(car2.CarMake);
+    }
+    public static void StudentLogic() {
+        Student student1 = new Student();
+        student1.SetFirstName("Kurt");
+        student1.SetLastName("Vonnegut");
+        student1.GPA = 4.0f;
+        student1.Major = Major.ART; 
+        Student student2 = new Student();
+        student2.SetFirstName("Ernest");
+        student2.SetLastName("Hemingway");
+        System.out.println(student1.getFirstName() + " " + student1.getLastName());
+        System.out.println(student2.getFirstName() + " " + student2.getLastName());
     }
 }
--- Student.java
+++ Student.java
@@ -1,7 +1,21 @@
 package CSC205_Week3_Monday;
 
 public class Student {
-    String FirstName, LastName;
+    private String FirstName, LastName;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+    public void SetFirstName(String NewName) {
+        FirstName = NewName.toLowerCase();
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public void SetLastName(String NewName) {
+        LastName = NewName.toUpperCase();
+    }
     float GPA;
     Major Major;
+    boolean isVeteran;
 }
